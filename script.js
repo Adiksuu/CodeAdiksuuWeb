ed032a1d6abd1da1e04af061d7a3ab25c23f279c
Adiksuu <75419729+Adiksuu@users.noreply.github.com>
2022-01-30 12:55:27 +0100

    Files for website

--- script.js
+++ script.js
@@ -15,6 +15,27 @@ let checkCookie = document.cookie.indexOf("CookieBy=CodeAdiksuu");
 checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
 
 
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
