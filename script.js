9119bc38ed2803bb2e2faf65f94f037aa96715bc
Adiksuu <75419729+Adiksuu@users.noreply.github.com>
2022-04-10 09:40:22 +0200

    Files For Website

--- script.js
+++ script.js
@@ -1,86 +1,72 @@
+const showMenu = (toggleId, navId) => {
+	const toggle = document.getElementById(toggleId),
+		nav = document.getElementById(navId);
 
-  let sidebar = document.querySelector(".sidebar");
-  let closeBtn = document.querySelector("#btn");
+	if (toggle && nav) {
+		toggle.addEventListener("click", () => {
+			nav.classList.toggle("show");
+		});
+	}
+};
 
+showMenu("nav-toggle", "nav-menu");
 
-  closeBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("open");
-    menuBtnChange();
-  });
+const navLink = document.querySelectorAll(".nav__link");
 
+function linkAction() {
+	navLink.forEach((n) => n.classList.remove("active"));
+	this.classList.add("active");
 
+	const navMenu = document.getElementById("nav-menu");
+	navMenu.classList.remove("show");
+}
 
-  function menuBtnChange() {
-   if(sidebar.classList.contains("open")){
-     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-   }else {
-     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
-   }
-  }
-
-
-
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 const cookieBox = document.querySelector(".wrapper"),
-acceptBtn = cookieBox.querySelector(".buttons button");
+	acceptBtn = cookieBox.querySelector(".buttons button");
 
-    acceptBtn.onclick = () => {
-         document.cookie = "CookieBy=CodeAdiksuu; max-age" + 60 * 60 * 24 * 30;
+acceptBtn.onclick = () => {
+	document.cookie = "CookieBy=CodeAdiksuu; max-age" + 60 * 60 * 24 * 30;
 
-        if (document.cookie) {
-        cookieBox.classList.add("hide");
-        } else {
-         alert("Cookies can't be set!");
-    }
-}
+	if (document.cookie) {
+		cookieBox.classList.add("hide");
+	} else {
+		alert("Cookies can't be set!");
+	}
+};
 
 let checkCookie = document.cookie.indexOf("CookieBy=CodeAdiksuu");
-checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
-
-
-
-const inputs = document.querySelectorAll(".input");
-
-function focusFunc() {
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
-
-function blurFunc() {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-}
-
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
-});
+checkCookie != -1
+	? cookieBox.classList.add("hide")
+	: cookieBox.classList.remove("hide");
 
 var icon = document.getElementById("dark-mode");
 
 icon.onclick = function () {
-    document.body.classList.toggle("dark-theme");
-
-    if (!document.body.classList.contains("dark-theme")) {
-        icon.src = "Images/moon.png";
-    } else {
-        icon.src = "Images/sun.png";
-    }
-}
-
-
+	document.body.classList.toggle("dark-theme");
+	document.getElementById("dark-mode").classList.toggle("dark");
+};
 
 const scrollBtn = document.querySelector(".scrollToTop-btn");
 
-window.addEventListener("scroll", function(){
-  scrollBtn.classList.toggle("active", window.scrollY > 500);
+window.addEventListener("scroll", function () {
+	scrollBtn.classList.toggle("active", window.scrollY > 500);
 });
-
 
 scrollBtn.addEventListener("click", () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 });
 
+const text = document.querySelector(".sec-text");
+
+const textLoad = () => {
+	setTimeout(() => {
+		text.textContent = "Web Designer";
+	}, 0);
+	
+};
+
+textLoad();
+setInterval(textLoad, 12000);
