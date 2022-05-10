822d047acaa3e5294841b74d7ac9febd71a359b4
Adiksuu <75419729+Adiksuu@users.noreply.github.com>
2022-05-10 18:11:48 +0200

    Files For Website

--- script.js
+++ script.js
@@ -68,3 +68,23 @@ function reveal(){
     }
   }
 }
+
+const sectionAll = document.querySelectorAll("section[id]");
+window.addEventListener("scroll", () => {
+	const scrollY = window.pageYOffset;
+	sectionAll.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 100;
+		const sectionId = current.getAttribute("id");
+		console.log(sectionId);
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector('li a[href*="' + sectionId + '"]')
+				.classList.add("active");
+		} else {
+			document
+				.querySelector('li a[href*="' + sectionId + '"]')
+				.classList.remove("active");
+		}
+	});
+});
