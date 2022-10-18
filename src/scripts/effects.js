d6f48541413898eff44b91159b31b2d28e7e8544
Adiksuu <75419729+Adiksuu@users.noreply.github.com>
2022-10-18 17:10:38 +0200

    Added a scroll-to-top button

--- src/scripts/effects.js
+++ src/scripts/effects.js
@@ -42,3 +42,14 @@ function rotate() {
 		}
 	}
 }
+
+const scrollBtn = document.querySelector(".stt");
+
+window.addEventListener("scroll", function () {
+	scrollBtn.classList.toggle("active", window.scrollY > 100);
+});
+
+scrollBtn.addEventListener("click", () => {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+});
