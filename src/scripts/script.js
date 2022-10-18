7f7f2e5729dbac87aadb9b9861a4be582a620cae
Adiksuu <75419729+Adiksuu@users.noreply.github.com>
2022-10-18 09:46:38 +0200

    Fixed - shop doesn't work

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -17,39 +17,3 @@ const hamburgerMenu = document.querySelector(".hamburger");
 hamburgerMenu.addEventListener("click", function () {
 	hamburgerMenu.classList.toggle("active");
 });
-
-const basicButton = document.querySelector("#basic");
-const advancedButton = document.querySelector("#advanced");
-const proButton = document.querySelector("#pro");
-const avatarButton = document.querySelector("#avatar");
-const thumbnailButton = document.querySelector("#thumbnail");
-
-basicButton.addEventListener("click", function () {
-	window.open(
-		"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=X2F5Y9PL2CJMA",
-		"target=_blank"
-	);
-});
-advancedButton.addEventListener("click", function () {
-	window.open(
-		"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BZ3Y7ZLNTM9KY",
-		"target=_blank"
-	);
-});
-proButton.addEventListener("click", function () {
-	window.open(
-		"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=B46ZYRCGAVFWE",
-		"target=_blank"
-	);
-});
-avatarButton.addEventListener("click", function () {
-	window.open(
-		"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NRYALZSL8KC4U",
-		"target=_blank"
-	);
-});
-thumbnailButton.addEventListener("click", function () {
-	window.open(
-		"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KLPK3UR7N8U5N"
-	);
-});
